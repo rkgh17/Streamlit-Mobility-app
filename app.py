7ceb3e5d4f52b0a47f3e1715500027bfbbc4af1b
황지훈 <rkgh17@gmail.com>
2022-12-02 17:30:30 +0900

    go home

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from pathlib import Path
+from seoul_bike_app import run_seoul_bike_app
 
 def main():
 
     with st.sidebar:
-        selected = option_menu(None, ["Home", 'Settings'], 
-        icons=['house', 'gear'], 
+        selected = option_menu(None, ["Home", '따릉이'], 
+        icons=['house', 'bicycle'], 
         menu_icon="list", 
         default_index=1, 
         styles={
@@ -23,7 +24,8 @@ def main():
         home = Path("md/home.md").read_text(encoding='UTF-8')
         st.markdown(home, unsafe_allow_html=True)
 
-    
+    if selected == "따릉이":
+        run_seoul_bike_app()
 
 if __name__ == "__main__":
     main()
---
+++ seoul_bike_app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+def run_seoul_bike_app():
+    st.write("hello")
