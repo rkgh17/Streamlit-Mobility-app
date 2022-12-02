d0c5eb87ba68a7b46db3c944d40f375d38bc26d3
황지훈 <rkgh17@gmail.com>
2022-12-02 11:00:22 +0900

    layout

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-#기본 형식
 import streamlit as st
+import sys
+sys.path.insert(1, "C:\Users\h\Desktop\ToyProject\Streamlit-Mobility-app\venv\Lib\site-packages\streamlit_option_menu")
 from streamlit_option_menu import option_menu
 from pathlib import Path
 
